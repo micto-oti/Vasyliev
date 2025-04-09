ed12936168275cc1f3b9715107ae197485c53330
micto <maksimka.vlasov.2006@gmail.com>
2025-04-09 23:13:58 +1000

    bgvc gbvxcbgdv

---
+++ Lab5/pipes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    /* long long M = 2, N = 5;
+
+
+
+
+
+    long long numerator = M * N;
+    long long denominator = M - N; */
+    
+    printf("Hello world!\n");
+
+
+    return 0;
+}
